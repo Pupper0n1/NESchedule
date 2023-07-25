bad8c8a6798e6192a938ed7b07f51c651e3edb6c
Wilbur <85022787+Pupper0n1@users.noreply.github.com>
2023-07-25 11:27:20 -0600

    Added filtering and search options in admin page

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -15,11 +15,13 @@ admin.site.index_title = "Welcome to The NESchedule Portal"
 class PersonFilter(admin.ModelAdmin):
     list_display = ('f_name', 'l_name', 'position', 'RTO_days', 'boutique')
     list_filter = ('boutique', 'position')
+    search_fields = ('f_name', 'l_name', 'boutique__name', 'boutique__city', 'boutique__province')
 
 
 class EventFilter(admin.ModelAdmin):
     list_display = ('person','type', 'date_start', 'date_end', 'time', 'status', 'boutique')
     list_filter = ('boutique', 'type', 'status')
+    search_fields = ('person__f_name', 'person__l_name', 'type', 'boutique__name', 'boutique__city', 'boutique__province')
 
 admin.site.register(Event, EventFilter)
 admin.site.register(Person, PersonFilter)
