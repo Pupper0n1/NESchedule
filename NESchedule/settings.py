c204fe8d76c99e9fe7048684587e7c1e9ec9f432
Wilbur <85022787+Pupper0n1@users.noreply.github.com>
2023-09-24 01:04:23 -0600

    updated STATIC_ROOT

--- NESchedule/settings.py
+++ NESchedule/settings.py
@@ -129,7 +129,7 @@ EMAIL_USE_TLS = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT='/home/pupper1n0/NESchedule/static/'
+STATIC_ROOT='/app/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
